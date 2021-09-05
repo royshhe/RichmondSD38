83049d525acaaadb45fd4a6d4d216b1c19f37581
Roy He <royshhe@hotmail.com>
2021-09-04 22:55:22 -0700

    Update Coin Denomination

--- CoinDenomination/Program.cs
+++ CoinDenomination/Program.cs
@@ -7,40 +7,75 @@ namespace CoinDenomination
     {
         static void Main(string[] args)
         {
-            LinkedList<int[]> lstPossibleWay = getPossibleWay(125);
-            foreach(int[] arr in lstPossibleWay)
+            LinkedList<int[]> lstPossibleWay = getPossibleWay(5);
+            Console.WriteLine($"There are {lstPossibleWay.Count} way");
+
+            Console.WriteLine("{1c, 2c, 5c, 10c, 20c, 50c, 1Euro, 2Euro}");
+            foreach (int[] arr in lstPossibleWay)
             {
-                Console.WriteLine("possible way:");
-                for (int i=0;i<arr.Length;i++)
-                Console.WriteLine(arr[i]);
+
+                //We could output this to a file
+                
+                Console.WriteLine(string.Join(",    ", arr));
             }
         }
         static LinkedList<int[]> getPossibleWay(int iAmount)
         {
             LinkedList<int[]> lstPossibleWay = new LinkedList<int[]>();
-            int iMaxQuarter, iMaxDime, iMaxNickel, iMaxPenny;          
-            
-            iMaxQuarter = (int)Math.Floor((double)iAmount / 25);
-            if (iMaxQuarter * 25 == iAmount) {                
-                 
-                lstPossibleWay.AddLast(new int[4] { 0, 0, 0, iMaxQuarter }); 
-            }
-            for (int i=0; i < iMaxQuarter; i++)
+            int iMax2Euro, iMax1Euro, iMax50c, iMax20c, iMaxDime, iMaxNickel,iMax2c, iMax1c;
+
+
+            iMax2Euro = (int)Math.Floor((double)iAmount / 200);
+            if (iMax2Euro * 200 == iAmount)
             {
-                iMaxDime =(int) Math.Floor((double)(iAmount - (25 * i)) / 10);
-                if (iMaxDime * 10 + 25 * i == iAmount) lstPossibleWay.AddLast(new int[4] { 0, 0, iMaxDime, i });
-                for (int j = 0; j < iMaxDime; j++)
+
+                lstPossibleWay.AddLast(new int[8] { 0,0,0,0,0,0,0, iMax2Euro});
+            }
+            for (int a = 0; a <= iMax2Euro; a++)
+            {
+                iMax1Euro = (int)Math.Floor((double)(iAmount - (200 * a)) / 100);
+                if (100 * iMax1Euro + 200 * a == iAmount) lstPossibleWay.AddLast(new int[8] { 0, 0, 0, 0, 0, 0, iMax1Euro, a });
+                for (int b = 0; b <= iMax1Euro; b++)
                 {
-                    iMaxNickel = (int)Math.Floor((double)(iAmount - (25 * i)-(10*j)) / 5);
-                    if (5* iMaxNickel + 10*j + 25 * i == iAmount) lstPossibleWay.AddLast(new int[4] { 0, iMaxNickel, j, i });
-                    for (int k=0; k<iMaxNickel; k++)
+                    iMax50c = (int)Math.Floor((double)(iAmount - (200 * a) - (100 * b)) / 50);
+                    if (50 * iMax50c + 100 * b + 200 * a == iAmount) lstPossibleWay.AddLast(new int[8] { 0, 0, 0, 0, 0, iMax50c, b, a });
+                    for (int c = 0; c <= iMax50c; c++)
                     {
-                        iMaxPenny = iAmount - 25 * i - 10 * j - 5 * k;
-                        lstPossibleWay.AddLast(new int[4] { iMaxPenny, k, j, i });
+
+                        iMax20c = (int)Math.Floor((double)(iAmount - (200 * a) - (100 * b) - (50 * c)) / 20);
+                        if (20 * iMax20c + 50 * c + 100 * b + 200 * a == iAmount) lstPossibleWay.AddLast(new int[8] { 0, 0, 0, 0, iMax20c, c, b, a });
+                        for (int d = 0; d <= iMax20c; d++)
+                        {
+                            iMaxDime = (int)Math.Floor((double)(iAmount - (200 * a) - (100 * b) - (50 * c) - (20 * d)) / 10);
+                            if (10*iMaxDime + 20 * d + 50 * c + 100 * b + 200 * a == iAmount) lstPossibleWay.AddLast(new int[8] { 0, 0, 0, iMaxDime, d, c, b, a });
+                            for (int e = 0; e <= iMaxDime; e++)
+                            {
+
+                                iMaxNickel = (int)Math.Floor((double)(iAmount - (200 * a) - (100 * b) - (50 * c) - (20 * d) - (10 * e)) / 5);
+
+                                if (5* iMaxNickel  + 10 * e + 20 * d + 50 * c + 100 * b + 200 * a == iAmount) 
+                                    lstPossibleWay.AddLast(new int[8] { 0, 0, iMaxNickel, e, d, c, b, a });
+
+                                for (int f = 0; f <= iMaxNickel; f++)
+                                {
+                                    iMax2c = (int)Math.Floor((double)(iAmount - (200 * a) - (100 * b) - (50 * c) - (20 * d) - (10 * e) - (5 * f)) / 2);
+                                    if (iMax2c * 2 + f * 5 + 10 * e + 20 * d + 50 * c + 100 * b + 200 * a == iAmount) lstPossibleWay.AddLast(new int[8] { 0, iMax2c, f, e, d, c, b, a });
+                                    for (int g = 0; g <= iMax2c; g++)
+                                    {
+                                       
+                                        iMax1c = iAmount - (200 * a) - (100 * b) - (50 * c) - (20 * d) - (10 * e) - (5 * f) - (2 * g);
+                                        if (iMax1c>0)
+                                        {
+                                            lstPossibleWay.AddLast(new int[8] { iMax1c, g, f, e, d, c, b, a });
+                                        }
+                                        
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
-
             return lstPossibleWay;
         }
     }
