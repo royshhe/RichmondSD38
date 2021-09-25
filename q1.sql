80cfa41d981248a4038a6dd1ed94fb6f88b85f1d
Roy He <royshhe@hotmail.com>
2021-09-24 19:39:00 -0700

    Delete q1.sql

--- q1.sql
+++
@@ -1,8 +0,0 @@
- 
-Select max(applicationID) ApplicationID,AppTwn-max(applicationID) as TwinID from 
-(
-SELECT ApplicationID,   ApplicationID+TwinID AppTwn
-FROM   Sibblings
-)v
-Group by AppTwn
- 
